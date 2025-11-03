269fd9211a078b31b52953f64d0d8f8472f66b8b
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-11-04 00:41:25 +0500

    Primtives Done

--- src/02_primitives.ts
+++ src/02_primitives.ts
@@ -8,3 +8,10 @@ const big: bigint = 2n ** 63n - 1n;
 
 // Symbol Type
 const TOKEN: unique symbol = Symbol("TOKEN");
+
+function yearsToDay(years: number): number {
+    return years * 365;
+}
+
+console.log(username.toLocaleLowerCase());
+console.log(yearsToDay(20));
